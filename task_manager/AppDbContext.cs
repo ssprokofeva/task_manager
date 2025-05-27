2dddbbcaa4b82a96708f51dd6162be7989fea771
Софья Прокофьева <fine.prokofjeva@yandex.ru>
2025-05-27 20:27:09 +0300

    работа с бд

---
+++ task_manager/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_manager
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext() : base(" name = TaskManagerConnection");
+        public DbSet<Task> Tasks { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+            .Property(t => t.title())
+            .IsRequired()
+            .HasMaxLength(255);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
